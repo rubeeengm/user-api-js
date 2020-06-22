bb598f7d712d23ba579e6ca4c54eb2b560752a21
Ruben Malaga <rubeeengm@outlook.com>
2020-06-21 20:10:47 -0500

    Inicio de proyecto

---
+++ db/context.js
@@ -0,0 +1,24 @@
+module.exports = userData = [{
+  id: 1
+  , userName: "dnarvaez"
+  , name: "Douglas"
+  , email: "dnarvaez@gmail.com"
+  , password: "test"
+  , createdDate: new Date(2020, 05, 18)
+}
+, {
+  id: 2
+  , userName: "ohernandez"
+  , name: "Oscar"
+  , email: "ohernandez@gmail.com"
+  , password: "test"
+  , createdDate: new Date(2020, 05, 18)
+}
+, {
+  id: 3
+  , userName: "mtorres"
+  , name: "mireya"
+  , email: "mtorres@gmail.com"
+  , password: "test"
+  , createdDate: new Date(2020, 05, 18)
+}];
